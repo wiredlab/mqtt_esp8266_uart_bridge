8de0da5215bc685337646cf928811fdf0ee42cfa
Dan White <dan@whiteaudio.com>
2023-06-16 10:56:53 -0500

    Update whitespace and comments

--- mqtt_esp8266_uart_bridge.ino
+++ mqtt_esp8266_uart_bridge.ino
@@ -1,21 +1,16 @@
 /*
- Basic ESP8266 MQTT example
- This sketch demonstrates the capabilities of the pubsub library in combination
- with the ESP8266 board/library.
- It connects to an MQTT server then:
-  - publishes "hello world" to the topic "outTopic" every two seconds
-  - subscribes to the topic "inTopic", printing out any messages
-    it receives. NB - it assumes the received payloads are strings not binary
-  - If the first character of the topic "inTopic" is an 1, switch ON the ESP Led,
-    else switch it off
- It will reconnect to the server if the connection is lost using a blocking
- reconnect function. See the 'mqtt_reconnect_nonblocking' example for how to
- achieve the same result without blocking the main loop.
- To install the ESP8266 board, (using Arduino 1.6.4+):
-  - Add the following 3rd party board manager under "File -> Preferences -> Additional Boards Manager URLs":
-       http://arduino.esp8266.com/stable/package_esp8266com_index.json
-  - Open the "Tools -> Board -> Board Manager" and click install for the ESP8266"
-  - Select your ESP8266 in "Tools -> Board"
+ * ESP8266 - UART-MQTT bridge
+ *
+ * (C) 2023 Dan White <dan.white@valpo.edu>
+ *
+ * Take frames on the UART RX pin and publish them via MQTT.
+ * Subscribe to MQTT topic and send contents out UART TX pin.
+ * Perodically publish a /status message.
+ *
+ * See config-example.h for configuration constants then copy to config.h and
+ * edit as appropriate.
+ *
+ * GPL-3.0 License
 */
 
 #include <ESP8266WiFi.h>
@@ -31,7 +26,6 @@
 // https://github.com/arduino-libraries/NTPClient
 #include <NTPClient.h>
 
-
 // https://arduinojson.org/
 #include <ArduinoJson.h>
 
@@ -43,7 +37,6 @@
  */
 #include "config.h"
 
-
 /* https://github.com/fabianoriccardi/git-describe-arduino
  * add a GIT_VERSION string that is updated every compile event
  */
@@ -51,7 +44,7 @@
 
 
 /*
- * globals
+ * Globals
  */
 String topic;
 struct tm timeinfo;
@@ -71,8 +64,9 @@ unsigned long inPackets = 0;
 unsigned long outPackets = 0;
 bool cold_boot = true;  // treat power-on differently
 
+
 WiFiClient wifi;
-ESP8266WiFiMulti wifiMulti;  // use multiple wifi options
+ESP8266WiFiMulti wifiMulti;
 WiFiUDP udp;
 NTPClient ntpClient(udp, NTP_SERVER, 0, NTP_UPDATE_INTERVAL);
 PubSubClient mqtt(wifi);
@@ -81,6 +75,12 @@ unsigned long last_mqtt_check = 0;
 
 
 
+/*
+ * Read a line from the UART port.
+ *
+ * Assumes that the bytes of a frame are sent within CHAR_TIMEOUT of each
+ * other, and that the frame is terminated by a newline character '\n'.
+ */
 String serialRead() {
   String buf = "";
   char inChar;
@@ -101,6 +101,7 @@ String serialRead() {
 }
 
 
+
 /*
  * Return a string in RFC3339 format of the current time.
  * Will return a placeholder if there is no network connection to an
@@ -114,12 +115,14 @@ String getIsoTime()
   localtime_r(&time_now, &timeinfo);
 
   if (timeinfo.tm_year <= (2016 - 1900)) {
+    // seems to be a bogus value, so return a placeholder
     return String("YYYY-MM-DDTHH:MM:SSZ");
   } else {
     strftime(timeStr, sizeof(timeStr), "%Y-%m-%dT%H:%M:%SZ", &timeinfo);
     return String(timeStr);
   }
 }
+
 
 
 /*
